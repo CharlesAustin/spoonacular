a11bb0436aaf8e522a0dd007eb705cc0e07fbfcc
Austin <austinatk22@gmail.com>
2023-02-28 16:21:58 -0500

    init commit

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// DETAILS PAGE req
+// Recipe name
+// Recipe image
+// Health info
+// list of ingredients
+// cooking instructions
+// For each ingredient: display ingredient name and measure
+
+export default function Detail() {
+    return (
+        <div>This is the details page</div>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// HOMEPAGE reqs
+// search input
+// search button (execute on enter/button click)
+// search results with pagination (5/page)
+// each search result should display: recipe name, recipe image
+// link to recipe detail page
+
+export default function Home() {
+    
+}
